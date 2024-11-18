ef3d6642e4b908811e41b4cda0f333ae007336a3
Laurent Sercomanens <aurovici@gmail.com>
2024-11-18 16:59:17 +0100

    add rubocop.yml file and continue with the calculator app

--- RB101/lesson_1/calculator.rb
+++ RB101/lesson_1/calculator.rb
@@ -1,23 +1,178 @@
-Kernel.puts("Welcome to the calculator program!")
-Kernel.puts("----------------------------------")
-Kernel.puts("I need two numbers from you")
-Kernel.puts("---------------------------")
-Kernel.puts("Type the first number: ")
-num1 = Kernel.gets().chomp().to_i()
-Kernel.puts("Type the second number: ")
-num2 = Kernel.gets().chomp().to_i()
-Kernel.puts("Now type the operation you want to perform")
-Kernel.puts("1. to add, 2. to subtract, 3. to multiply, 4. to divide")
-operation = Kernel.gets().chomp().to_i()
-
-if operation == 1
-  Kernel.puts("The result is #{num1 + num2}")
-elsif operation == 2
-  Kernel.puts("The result is #{num1 - num2}")
-elsif operation == 3
-  Kernel.puts("The result is #{num1 * num2}")
-elsif operation == 4
-  Kernel.puts("The result is #{num1.to_f() / num2.to_f()}")
-else
-  Kernel.puts("Invalid operation")
+def prompt(message)
+  Kernel.puts("=> #{message}")
 end
+
+def valid_number?(num)
+  num.to_i() != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    "Adding"
+  when '2'
+    "Subtracting"
+  when '3'
+    "Multiplying"
+  when '4'
+    "Dividing"
+  end
+end
+
+prompt("Welcome to the calculator program! Please enter your name:")
+name = nil
+loop do
+  name = Kernel.gets().chomp()
+
+  if name.empty?()
+    prompt("Make sure to use a valid name")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}!")
+
+loop do
+  Kernel.puts("----------------------------------")
+  prompt("I need two numbers from you")
+  Kernel.puts("---------------------------")
+  num1 = nil
+  loop do
+    prompt("Type the first number: ")
+    num1 = Kernel.gets().chomp()
+
+    if valid_number?(num1)
+      break
+    else
+      prompt("Invalid number. Please try again")
+    end
+  end
+
+  prompt("Type the second number: ")
+  num2 = nil
+  loop do
+    num2 = Kernel.gets().chomp()
+
+    if valid_number?(num2)
+      break
+    else
+      prompt("Invalid number. Please try again")
+    end
+  end
+
+  prompt("Now type the operation you want to perform")
+  operator_prompts = <<-MSG
+  What operation would you like to perform?
+  1. to add
+  2. to subtract
+  3. to multiply
+  4. to divide
+  MSG
+
+  prompt(operator_prompts)
+
+  operation = nil
+  loop do
+    operation = Kernel.gets().chomp()
+    if %w(1 2 3 4).include?(operation)
+      break
+    else
+      prompt("Invalid operation. Please choose: 1,2,3 or 4 only...")
+    end
+  end
+
+  prompt("#{operation_to_message(operation)} the two numbers...")
+
+  result = case operation
+  when '1'
+    prompt("The result is #{num1.to_i + num2.to_i}")
+  when '2'
+    prompt("The result is #{num1.to_i - num2.to_i}")
+  when '3'
+    prompt("The result is #{num1.to_i * num2.to_i}")
+  when '4'
+    prompt("The result is #{num1.to_f / num2.to_f}")
+  else
+    "Invalid operation"
+  end
+
+  prompt("Do you want to perform another calculation? (Y to calculate again)")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for using the calculator. Goodbye!")
+
+
+# def prompt(message)
+#   Kernel.puts("=> #{message}")
+# end
+
+# prompt("Welcome to the calculator program!")
+# Kernel.puts("----------------------------------")
+# prompt("I need two numbers from you")
+# Kernel.puts("---------------------------")
+# prompt("Type the first number: ")
+# num1 = Kernel.gets().chomp().to_i()
+# prompt("Type the second number: ")
+# num2 = Kernel.gets().chomp().to_i()
+# prompt("Now type the operation you want to perform")
+# prompt("1. to add, 2. to subtract, 3. to multiply, 4. to divide")
+# operation = Kernel.gets().chomp().to_i()
+
+# result = case operation
+# when 1
+#   prompt("The result is #{num1 + num2}")
+# when 2
+#   prompt("The result is #{num1 - num2}")
+# when 3
+#   prompt("The result is #{num1 * num2}")
+# when 4
+#   prompt("The result is #{num1.to_f() / num2.to_f()}")
+# else
+#   "Invalid operation"
+# end
+
+
+
+# if operation == 1
+#   prompt("The result is #{num1 + num2}")
+# elsif operation == 2
+#   prompt("The result is #{num1 - num2}")
+# elsif operation == 3
+#   prompt("The result is #{num1 * num2}")
+# elsif operation == 4
+#   prompt("The result is #{num1.to_f() / num2.to_f()}")
+# else
+#   prompt("Invalid operation")
+# end
+
+
+
+
+
+
+# Kernel.puts("Welcome to the calculator program!")
+# Kernel.puts("----------------------------------")
+# Kernel.puts("I need two numbers from you")
+# Kernel.puts("---------------------------")
+# Kernel.puts("Type the first number: ")
+# num1 = Kernel.gets().chomp().to_i()
+# Kernel.puts("Type the second number: ")
+# num2 = Kernel.gets().chomp().to_i()
+# Kernel.puts("Now type the operation you want to perform")
+# Kernel.puts("1. to add, 2. to subtract, 3. to multiply, 4. to divide")
+# operation = Kernel.gets().chomp().to_i()
+
+# if operation == 1
+#   Kernel.puts("The result is #{num1 + num2}")
+# elsif operation == 2
+#   Kernel.puts("The result is #{num1 - num2}")
+# elsif operation == 3
+#   Kernel.puts("The result is #{num1 * num2}")
+# elsif operation == 4
+#   Kernel.puts("The result is #{num1.to_f() / num2.to_f()}")
+# else
+#   Kernel.puts("Invalid operation")
+# end
